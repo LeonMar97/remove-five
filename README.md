dd77c878cba4c6de9b2f982d00a5bb3326c948c8
Leon Markovich <leonmarkovich97@gmail.com>
2023-11-10 16:48:23 +0200

    cleaned the code abit, and created a readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # remove-five
 An assignment, find out which '5' digit to remove from a number to get the largest number left.
+
+## algorithm explanation
+examining the locations of fives, we can adress '5' as cur_five and succeeding number right after the '5' as cur_next, when looking from msb to lsb. 
+if num is postive, we should remove the first 'cur_five' only if cur_next is larger than 5, making the cur_next first inline to the msb, should be bigger than the removed cur_five.
+
+if no larger cur_next found, we should create less "damage" to the remaining number thus- remove the lsb cur_five. 
+
+if 'num' is negative, we address the same msb, but upside down , 
+knowing that,in negative form the lower the number the bigger it is.
+
+we will do the same algorithm, but will seek the 'cur_next' which is
+smaller than 5.
+again we will make less dammage romving the lsb cur_five when negative if all the the succeeding numbers are bigger than 5.
+
--- rm-fives.py
+++ rm-fives.py
@@ -1,20 +1,12 @@
 def solution(num:int)->int:
     str_num=str(num)
     neg=True if str_num[0]=='-' else False
-    fives_loc=[i for i,j in enumerate(str_num) if j=='5']
-    if not neg:
-        for i in fives_loc:
-            if i<len(str_num)-2 and int(str_num[i+1])>5:
-                return int(str_num[:i]+str_num[i+1:])
-        return int(str_num[:fives_loc[-1]]+str_num[fives_loc[-1]+1:])
-    else:
-        for i in fives_loc:
-            if i<len(str_num)-2 and int(str_num[i+1])<5:
-                return int(str_num[:i]+str_num[i+1:])
-        return int(str_num[:fives_loc[-1]]+str_num[fives_loc[-1]+1:])
+    fives_loc=[i for i,j in enumerate(str_num) if j=='5'] 
+    for cur_five in fives_loc:
+        if cur_five<len(str_num)-2 and ((int(str_num[cur_five+1])>5 and not neg) or (int(str_num[cur_five+1])<5 and neg)):
+            return int(str_num[:cur_five]+str_num[cur_five+1:]) #cur_five+1 is cur_next explained in readme
+    return int(str_num[:fives_loc[-1]]+str_num[fives_loc[-1]+1:])
+   
 
 if __name__=="__main__":
-    print(solution(1525))
-    
-
-    
+    print(solution(-595859))
